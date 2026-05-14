c9536a23802ecd7a8a3118c3cfb5e045a0be6a70
PravallikaRoyal <pravallikaroyal2004@gmail.com>
2026-05-14 13:53:31 +0530

    Create trailers section and add footer using https://prebuiltui.com/

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import MovieCard from "../components/FeaturedSection";
 import FeaturedSection from "../components/FeaturedSection";
+import TrailersSection from "../components/TrailersSection";
 
 const Home = () =>{
     return(
         <>
         <HeroSection/>
         <FeaturedSection/>
+        <TrailersSection/>
         </>
     )
 }
