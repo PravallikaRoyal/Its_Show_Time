b3345de034b912c9010f7bee062f0819dfff36cc
PravallikaRoyal <pravallikaroyal2004@gmail.com>
2026-05-16 22:26:49 +0530

    Add movie details for every movie such as cast and booking date, develop seatlayout page for seat selection, create LoadingAni component for loading and DateSelect component for selecting dates to book the show

--- client/src/pages/Favourite.jsx
+++ client/src/pages/Favourite.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { dummyShowsData } from "../assets/assets";
+import MovieCard from "../components/MovieCard";
+import BlurRedCircle from "../components/BlurRedCircle";
 
 const Favourite = () =>{
-    return(
+    return dummyShowsData.length > 0 ? (
+        <div className='relative my-40 mb-60 px-6 md:px-16 
+        lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
+            <BlurRedCircle top="150px" left="0px"/>
+            <BlurRedCircle buttom="50px" right="50px"/>
+            <h1 className="text-lg font-medium my-4">Your Favourite Movies</h1>
+            <div className=" flex flex-wrap max-sm:justify-center gap-8">
+                {dummyShowsData.map((movie)=>(
+                    <MovieCard movie={movie} key = {movie._id}/>
+                )
+                )}
+            </div>
+            
+        </div>
+    ) : (
         <div>
+            <h1>No movies available</h1>
 
         </div>
     )
