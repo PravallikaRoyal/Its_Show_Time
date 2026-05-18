c60b907c71b87bd604593ab3a28c3ca0c8bc3166
PravallikaRoyal <pravallikaroyal2004@gmail.com>
2026-05-18 22:07:41 +0530

    Create admin pages: AddShows.jsx, Dashboard.jsx, Layout.jsx, ListBookings.jsx, ListShows.jsx

---
+++ client/src/components/admin/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({text1, text2}) => {
+  return (
+    <h1 className='font-medium text-2xl text-primary'>
+        {text1} {text2}
+    </h1>
+  )
+}
+
+export default Title
