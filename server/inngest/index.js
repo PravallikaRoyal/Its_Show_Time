d38ff81342c7b1bb379b73929a919a5819bb86db
PravallikaRoyal <pravallikaroyal2004@gmail.com>
2026-05-29 23:04:35 +0530

    Clear server crash error

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -114,8 +114,10 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
 
 // Inngest Function to send email when user books a show
 const sendBookingConfirmationEmail = inngest.createFunction(
-  { id: "send-booking-confirmation-email" },
-  { event: "app/show.booked" },
+  {
+    id: "send-booking-confirmation-email",
+    triggers: [{ event: "app/show.booked" }]
+  },
   async ({ event, step }) => {
     const { bookingId } = event.data;
 
